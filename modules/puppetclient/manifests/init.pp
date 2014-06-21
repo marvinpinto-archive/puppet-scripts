2fa646a8d17f3607bbcfcc2951c9fcc4ce6ca999
Marvin Pinto <me@marvinp.ca>
2014-06-21 12:55:43 -0400

    Effectively separate puppet private and public.

--- modules/puppetclient/manifests/init.pp
+++ modules/puppetclient/manifests/init.pp
@@ -1,7 +1,16 @@
 class puppetclient(
   $pupstop_file,
-  $confdir,
+  $private_confdir,
+  $public_confdir,
+  $deployment_private_key_contents,
+  $deployment_private_key_loc,
+  $public_repo_url,
+  $private_repo_url,
+  $git_puppet_ssh_wrapper_loc,
 ) {
+
+  # The utils class contains git, amongst other things
+  require 'utils'
 
   # Ensure that we have the newest available version of puppet
   package {'puppet':
@@ -42,6 +51,37 @@ class puppetclient(
     mode    => '0755',
     content => template('puppetclient/puprun.erb'),
     require => Package['puppet'],
+  }
+
+  # Private key used to git-pull puppet config
+  file { 'puppet-deployment-key':
+    ensure  => present,
+    path    => $deployment_private_key_loc,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    content => $deployment_private_key_contents,
+  }
+
+  # Git wrapper around ssh in order to use specific private key files
+  file { 'git-puppet-ssh':
+    ensure  => present,
+    path    => $git_puppet_ssh_wrapper_loc,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
+    content => template('puppetclient/git_puppet_ssh.erb'),
+    require => File['puppet-deployment-key'],
+  }
+
+  file { 'puppull':
+    ensure  => present,
+    path    => '/usr/local/bin/puppull',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('puppetclient/puppull.erb'),
+    require => File['git-puppet-ssh'],
   }
 
 }
