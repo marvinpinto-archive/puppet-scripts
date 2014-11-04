916958523d484b5035fb2775239b414d02254bc4
Marvin Pinto <me@marvinp.ca>
2014-11-04 08:12:22 -0500

    Add media post processing module

---
+++ modules/nzb_post_processing/manifests/init.pp
@@ -0,0 +1,32 @@
+class nzb_post_processing (
+  $base_directory,
+  $media_group,
+  $bt_autoprocess_output_dir,
+) {
+
+  file { $base_directory:
+    ensure => directory,
+    owner  => 'root',
+    group  => $media_group,
+    mode   => '0775',
+  }
+
+  vcsrepo { $base_directory:
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/clinton-hall/nzbToMedia.git',
+    user     => 'root',
+    group    => $media_group,
+    require  => File[$base_directory],
+  }
+
+  file { "${base_directory}/autoProcessMedia.cfg":
+    ensure  => file,
+    owner   => 'root',
+    group   => $media_group,
+    mode    => '0644',
+    content => template('nzb_post_processing/autoProcessMedia.cfg.erb'),
+    require => Vcsrepo[$base_directory],
+  }
+
+}
